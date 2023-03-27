019cbcd58676eb7ea9925935796447a8004f7c86
lukasu92 <root@Lucas>
2023-03-27 18:35:22 -0300

    inclusão de um contador de tasks e pop up de confirmação para exclusão das tasks.

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function addNewTask() {
     let btn2 = document.createElement("button");
     if (newTask === "") {
         document.getElementById("msg1").style.display = "block";
-        const timeOut = setTimeout(hideMsg, 3000);       
+        const timeOut = setTimeout(hideMsg, 3000);
     } else {
         span.appendChild(txt);
         span.setAttribute("id", "convt");
@@ -21,6 +21,8 @@ function addNewTask() {
         document.getElementById("tasks").appendChild(li);
         document.getElementById("newTask").value = "";
         document.getElementById("newTask").focus();
+        
+        counter();
     }
 }
 
@@ -29,57 +31,73 @@ addEventListener("click", (ev) => {
     if (ev.target.nodeName === "SPAN") {
         if (ev.target.className === "") {
             ev.target.className = "checked";
-            ev.target.parentNode.className = "checked";
+            ev.target.parentNode.className = "checked";         
         } else {
             ev.target.className = "";
             ev.target.parentNode.className = "";
         }
+        counter();
     }
 });
 
 //DELETE
+let evTarget = "";
 addEventListener("click", (ev) => {
     if (ev.target.className === "delTask") {
-        const evTarget = ev.target;
-        evTarget.parentNode.remove(evTarget);
-    }
+        document.getElementById("overlay").hidden = false;
+        evTarget = ev.target;
+    }    
 });
 
+function isConfirm(answer) {
+    if (answer) {        
+        evTarget.parentNode.remove(evTarget);
+    }
+    closeConfirmBox();
+    counter();
+}
+
+function closeConfirmBox() {
+    document.getElementById("overlay").hidden = true;
+}
+
 //EDIT
-//posicinar cursor no final da palavra
 addEventListener("click", (ev) => {
     if (ev.target.className === "editTask") {
-        let span = document.getElementById("convt");
-        if (span.className != "checked") {
-            let txt = ev.target.parentNode.childNodes[0].innerText;
-            span.innerHTML = '<input id="txtTask" onfocus="this.selectionStart = this.selectionEnd = 500;" onblur="spanReset(this)" type="text" value="' + txt + '"/>';
-            document.getElementById("txtTask").focus();    
-        }else{
-            document.getElementById("msg2").style.display = "block";
-            const timeOut = setTimeout(hideMsg, 3000);
-
-        }      
+        let span = ev.target.parentNode.childNodes[0];
+        let txt = ev.target.parentNode.childNodes[0].innerText;
+        span.innerHTML = '<input id="txtTask" onfocus="this.selectionStart = this.selectionEnd = 500;" onblur="spanReset(this)" type="text" value="' + txt + '"/>';
+        document.getElementById("txtTask").focus();
     }
 });
 
 function spanReset(e) {
-    let span = document.getElementById("convt");
-    let txt = document.getElementById("txtTask").value;
+    let span = e.parentNode;
+    let txt = e.value;
     if (txt === "") {
         document.getElementById("txtTask").focus();
         document.getElementById("msg1").style.display = "block";
-        const timeOut = setTimeout(hideMsg, 3000);       
+        const timeOut = setTimeout(hideMsg, 3000);
     } else {
         span.innerHTML = txt;
     }
 }
 
 function hideMsg() {
-    
+
     if (document.getElementById("msg1")) {
         document.getElementById("msg1").style.display = "none";
     }
     if (document.getElementById("msg2")) {
         document.getElementById("msg2").style.display = "none";
     }
+}
+
+function counter() {
+    let check = document.querySelectorAll("li.checked");
+    let tasks = document.getElementsByTagName("li");
+    let prog = document.getElementById("progress");
+
+    prog.innerHTML = check.length + "/" + tasks.length;
+    console.log(check);
 }
